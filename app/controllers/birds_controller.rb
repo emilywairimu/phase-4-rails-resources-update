a45df3b9b59e5376cf71816341dd1f246bf0217b
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-06-23 04:32:52 -0500

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,27 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+# PATCH /birds/:id
+def update
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(bird_params)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+end
+
+def increment_likes
+  bird = Bird.find_by(id: params[:id])
+  if bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+  else
+    render json: { error: "Bird not found" }, status: :not_found
+  end
+end
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
